0fbde7da57bf4f24fbf051a04f5580c79ced7d88
Mark24355 <markwagih243@gmail.com>
2025-05-17 15:03:05 +0300

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+$num1 = 10;
+$num2 = 5;
+
+
+$sum = $num1 + $num2;         
+$difference = $num1 - $num2;  
+$product = $num1 * $num2;      
+$quotient = $num1 / $num2;    
+$remainder = $num1 % $num2;   
+
+echo "Addition: $num1 + $num2 = $sum\n";
+echo "Subtraction: $num1 - $num2 = $difference\n";
+echo "Multiplication: $num1 * $num2 = $product\n";
+echo "Division: $num1 / $num2 = $quotient\n";
+echo "Modulus: $num1 % $num2 = $remainder\n";
+?>
+
